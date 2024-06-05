47637e18736385aa91e6305799b2a44d27fbdf6e
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-06-05 15:53:01 +0600

    Features: Home Page Implementedddd

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,5 +1,9 @@
 const Container = ({ children }) => {
-	return <div className="w-full md:w-5/6 mx-auto px-5 md:px-0">{children}</div>;
+	return (
+		<div className="w-full md:w-5/6 mx-auto px-5 md:px-0 h-screen flex flex-col justify-center items-center">
+			{children}
+		</div>
+	);
 };
 
 export default Container;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import logo from "../assets/images/logo.jpeg";
 import Container from "../components/container/Container";
+import Button from "../components/utils/Button";
 
 const Home = () => {
-	return <Container>Home</Container>;
+	return (
+		<Container>
+			<div className="w-[60%] mx-auto flex flex-col justify-center items-center">
+				<img src={logo} className="w-full mb-5" alt="" />
+				<Button btnTitle="Start" />
+			</div>
+		</Container>
+	);
 };
 
 export default Home;
