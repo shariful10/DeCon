1222dce950b256ae315d6fd2d0e471c63e9d5fda
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-06-05 16:39:45 +0600

    Refector: Navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -5,13 +5,11 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
 	return (
-		<Container>
-			<div className="py-5">
-				<Link to="/">
-					<img src={logo} className="w-[120px]" alt="" />
-				</Link>
-			</div>
-		</Container>
+		<div className="px-5 md:px-20 py-5">
+			<Link to="/">
+				<img src={logo} className="w-[120px]" alt="" />
+			</Link>
+		</div>
 	);
 };
 
