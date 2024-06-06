d5a529c3e42d96bfb2a0feb70fb57db4b942612d
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-06-07 00:53:32 +0600

    Refector: BuildingInfo Page, Features: Uploaded Image Preview

--- src/pages/BuildingInfo.jsx
+++ src/pages/BuildingInfo.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/utils/Button";
 import Navbar from "../components/shared/Navbar";
@@ -7,7 +8,6 @@ import UploadIcon from "../components/icons/UploadIcon";
 import Container from "../components/container/Container";
 import { addBuildingInfo } from "../redux/features/buildingInfoSlice";
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
 
 const BuildingInfo = () => {
 	const dispatch = useDispatch();
@@ -15,9 +15,9 @@ const BuildingInfo = () => {
 	const { buildingInfo } = useSelector((state) => state.buildingInfo);
 	console.log(
 		"🚀 ~ file: BuildingInfo.jsx:16 ~ BuildingInfo ~ buildingInfo:",
-		buildingInfo
+		buildingInfo.country
 	);
-
+	const [imagePreview, setImagePreview] = useState(null);
 	const [building, setBuilding] = useState({
 		buildingType: "",
 		country: "",
@@ -31,6 +31,28 @@ const BuildingInfo = () => {
 		softwareVersion: "",
 		image: "",
 	});
+
+	useEffect(() => {
+		if (buildingInfo) {
+			setBuilding(buildingInfo);
+		}
+	}, [buildingInfo]);
+
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setBuilding({
+			...building,
+			[name]: value,
+		});
+	};
+
+	const handleImageChange = (e) => {
+		const file = e.target.files[0];
+		if (file) {
+			setBuilding({ ...building, image: file });
+			setImagePreview(URL.createObjectURL(file));
+		}
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -60,16 +82,14 @@ const BuildingInfo = () => {
 											</div>
 											<div className="col-span-2">
 												<input
-													onChange={(e) =>
-														setBuilding({
-															...building,
-															buildingType: e.target.value,
-														})
-													}
+													onChange={handleInputChange}
 													type="text"
 													className="bg-gray-200 py-1.5 pl-3 focus:outline-none w-full"
 													name="buildingType"
 													id="buildingType"
+													value={
+														building.buildingType || buildingInfo.buildingType
+													}
 													required
 												/>
 											</div>
@@ -83,16 +103,12 @@ const BuildingInfo = () => {
 											<div className="col-span-2">
 												<div className="flex justify-between items-end gap-4">
 													<input
-														onChange={(e) =>
-															setBuilding({
-																...building,
-																country: e.target.value,
-															})
-														}
+														onChange={handleInputChange}
 														type="text"
 														className="bg-gray-200 py-1.5 pl-3 focus:outline-none w-[35%]"
 														name="country"
 														id="country"
+														value={building.country || buildingInfo.country}
 														required
 													/>
 													<div className="flex items-end justify-between gap-4">
@@ -103,16 +119,12 @@ const BuildingInfo = () => {
 															Post code:
 														</label>
 														<input
-															onChange={(e) =>
-																setBuilding({
-																	...building,
-																	postCode: e.target.value,
-																})
-															}
+															onChange={handleInputChange}
 															type="text"
 															className="bg-gray-200 py-1.5 pl-3 focus:outline-none w-full"
 															name="postCode"
 															id="postCode"
+															value={building.postCode || buildingInfo.postCode}
 															required
 														/>
 													</div>
@@ -128,16 +140,12 @@ const BuildingInfo = () => {
 											<div className="col-span-2">
 												<div className="flex justify-between items-end gap-4">
 													<input
-														onChange={(e) =>
-															setBuilding({
-																...building,
-																city: e.target.value,
-															})
-														}
+														onChange={handleInputChange}
 														type="text"
 														className="bg-gray-200 py-1.5 pl-3 focus:outline-none w-[55%]"
 														name="city"
 														id="city"
+														value={building.city || buildingInfo.city}
 														required
 													/>
 													<div className="flex items-end justify-between gap-4">
@@ -150,16 +158,12 @@ const BuildingInfo = () => {
 																	Street:
 																</label>
 																<input
-																	onChange={(e) =>
-																		setBuilding({
-																			...building,
-																			street: e.target.value,
-																		})
-																	}
+																	onChange={handleInputChange}
 																	type="text"
 																	className="bg-gray-200 py-1.5 pl-3 focus:outline-none w-full"
 																	name="street"
 																	id="street"
+																	value={building.street || buildingInfo.street}
 																	required
 																/>
 															</div>
@@ -171,16 +175,12 @@ const BuildingInfo = () => {
 																	No:
 																</label>
 																<input
-																	onChange={(e) =>
-																		setBuilding({
-																			...building,
-																			no: e.target.value,
-																		})
-																	}
+																	onChange={handleInputChange}
 																	type="number"
 																	className="bg-gray-200 py-1.5 pl-3 focus:outline-none w-full"
 																	name="no"
 																	id="no"
+																	value={building.no || buildingInfo.no}
 																	required
 																/>
 															</div>
@@ -198,16 +198,12 @@ const BuildingInfo = () => {
 											<div className="col-span-2">
 												<div className="flex justify-between items-end gap-4">
 													<input
-														onChange={(e) =>
-															setBuilding({
-																...building,
-																area: e.target.value,
-															})
-														}
+														onChange={handleInputChange}
 														type="text"
 														className="bg-gray-200 py-1.5 pl-3 focus:outline-none w-[35%]"
 														name="area"
 														id="area"
+														value={building.area || buildingInfo.area}
 														required
 													/>
 												</div>
@@ -225,16 +221,15 @@ const BuildingInfo = () => {
 											<div className="col-span-2">
 												<div className="flex justify-between items-end gap-4">
 													<input
-														onChange={(e) =>
-															setBuilding({
-																...building,
-																constructionDate: e.target.value,
-															})
-														}
+														onChange={handleInputChange}
 														type="date"
 														className="bg-gray-200 py-1.5 px-3 focus:outline-none w-[35%]"
 														name="constructionDate"
 														id="constructionDate"
+														value={
+															building.constructionDate ||
+															buildingInfo.calculationDate
+														}
 														required
 													/>
 												</div>
@@ -252,16 +247,15 @@ const BuildingInfo = () => {
 											<div className="col-span-2">
 												<div className="flex justify-between items-end gap-4">
 													<input
-														onChange={(e) =>
-															setBuilding({
-																...building,
-																calculationDate: e.target.value,
-															})
-														}
+														onChange={handleInputChange}
 														type="date"
 														className="bg-gray-200 py-1.5 px-3 focus:outline-none w-[35%]"
 														name="calculationDate"
 														id="calculationDate"
+														value={
+															building.calculationDate ||
+															buildingInfo.calculationDate
+														}
 														required
 													/>
 												</div>
@@ -279,16 +273,15 @@ const BuildingInfo = () => {
 											<div className="col-span-2">
 												<div className="flex justify-between items-end gap-4">
 													<input
-														onChange={(e) =>
-															setBuilding({
-																...building,
-																softwareVersion: e.target.value,
-															})
-														}
+														onChange={handleInputChange}
 														type="text"
 														className="bg-gray-200 py-1.5 px-3 focus:outline-none w-[35%]"
 														name="softwareVersion"
 														id="softwareVersion"
+														defaultValue={
+															building.softwareVersion ||
+															buildingInfo.softwareVersion
+														}
 														required
 													/>
 												</div>
@@ -297,28 +290,36 @@ const BuildingInfo = () => {
 									</div>
 								</div>
 								{/* Image upload from here */}
-								<div className="border-2 border-gray-300 relative p-10">
+								<div
+									className={`border-2 border-gray-300 relative ${
+										imagePreview ? "flex items-center p-5" : "p-10"
+									}`}
+								>
 									<input
-										onChange={(e) =>
-											setBuilding({
-												...building,
-												image: e.target.value,
-											})
-										}
+										onChange={handleImageChange}
 										type="file"
 										name="image"
 										id="image"
 										className="opacity-0 absolute top-0 left-0 w-full h-full"
 										accept="image/*"
-										required
 									/>
-									<label
-										htmlFor="image"
-										className="flex flex-col items-center justify-center w-full h-full cursor-pointer"
-									>
-										<UploadIcon />
-										<span className="text-gray-400">Upload building image</span>
-									</label>
+									{imagePreview ? (
+										<img
+											src={imagePreview}
+											alt="Building Preview"
+											className="w-full rounded-lg"
+										/>
+									) : (
+										<label
+											htmlFor="image"
+											className="flex flex-col items-center justify-center w-full h-full cursor-pointer"
+										>
+											<UploadIcon />
+											<span className="text-gray-400">
+												Upload building image
+											</span>
+										</label>
+									)}
 								</div>
 							</div>
 							<div className="text-right mt-[100px]">
