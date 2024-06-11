acd922ef40ad84ae76c6ab196b5abe8ce56a8483
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-06-11 11:58:06 +0600

    Features: Gauge Chart Implemented

--- src/pages/BuildingInfo.jsx
+++ src/pages/BuildingInfo.jsx
@@ -7,58 +7,60 @@ import Navbar from "../components/shared/Navbar";
 import UploadIcon from "../components/icons/UploadIcon";
 import Container from "../components/container/Container";
 import { addBuildingInfo } from "../redux/features/buildingInfoSlice";
+import Gauge from "../components/utils/Gauge";
 
 const BuildingInfo = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const { buildingInfo } = useSelector((state) => state.buildingInfo);
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const { buildingInfo } = useSelector((state) => state.buildingInfo);
 
-  const [imagePreview, setImagePreview] = useState(null);
-  const [building, setBuilding] = useState({
-    buildingType: "",
-    country: "",
-    postCode: "",
-    city: "",
-    street: "",
-    no: 0,
-    area: "",
-    constructionDate: "",
-    calculationDate: "",
-    softwareVersion: "",
-    image: "",
-  });
+	const [imagePreview, setImagePreview] = useState(null);
+	const [building, setBuilding] = useState({
+		buildingType: "",
+		country: "",
+		postCode: "",
+		city: "",
+		street: "",
+		no: 0,
+		area: "",
+		constructionDate: "",
+		calculationDate: "",
+		softwareVersion: "",
+		image: "",
+	});
 
-  useEffect(() => {
-    if (buildingInfo) {
-      setBuilding(buildingInfo);
-    }
-  }, [buildingInfo]);
+	useEffect(() => {
+		if (buildingInfo) {
+			setBuilding(buildingInfo);
+		}
+	}, [buildingInfo]);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setBuilding({
-      ...building,
-      [name]: value,
-    });
-  };
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setBuilding({
+			...building,
+			[name]: value,
+		});
+	};
 
-  const handleImageChange = (e) => {
-    const file = URL.createObjectURL(e.target.files[0]);
-    if (file) {
-      setBuilding({ ...building, image: file });
-      setImagePreview(file);
-    }
-  };
+	const handleImageChange = (e) => {
+		const file = URL.createObjectURL(e.target.files[0]);
+		if (file) {
+			setBuilding({ ...building, image: file });
+			setImagePreview(file);
+		}
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(addBuildingInfo(building));
-    navigate("/constructions-type");
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		dispatch(addBuildingInfo(building));
+		navigate("/constructions-type");
+	};
 
 	return (
 		<>
 			<Container>
+				<Gauge value={70} />
 				<div className="w-full md:w-[70%] mx-auto my-[50px]">
 					<div className="w-3/4 my-5"></div>
 					<h1 className="text-2xl font-semibold">Building information:</h1>
