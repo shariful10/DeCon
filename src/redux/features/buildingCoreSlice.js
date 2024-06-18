0ade5c29c08a4b1b1a05b4b6e2a698bde16afd3d
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-06-18 18:54:43 +0600

    rewuyg8ruwyg8iuwr

--- src/redux/features/buildingCoreSlice.js
+++ src/redux/features/buildingCoreSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
 	buildingCore: JSON.parse(localStorage.getItem("buildingCore")) || {},
+	buildingCoreInfo: JSON.parse(localStorage.getItem("buildingCoreInfo")) || {},
 };
 
 export const buildingCoreSlice = createSlice({
@@ -12,8 +13,16 @@ export const buildingCoreSlice = createSlice({
 			state.buildingCore = action.payload;
 			localStorage.setItem("buildingCore", JSON.stringify(state.buildingCore));
 		},
+		addBuildingCoreInfo: (state, action) => {
+			state.buildingCoreInfo = action.payload;
+			localStorage.setItem(
+				"buildingCoreInfo",
+				JSON.stringify(state.buildingCoreInfo)
+			);
+		},
 	},
 });
 
-export const { addBuildingCore } = buildingCoreSlice.actions;
+export const { addBuildingCore, addBuildingCoreInfo } =
+	buildingCoreSlice.actions;
 export default buildingCoreSlice.reducer;
