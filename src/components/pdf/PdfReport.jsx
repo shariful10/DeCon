c94a5bfe328d4adac892661c9a0c6d02d2b1d16c
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-06-29 18:17:36 +0600

    Refector: Pdf file Modify

--- src/components/pdf/PdfReport.jsx
+++ src/components/pdf/PdfReport.jsx
@@ -1,20 +1,32 @@
 import { useRef } from "react";
 import Button from "../utils/Button";
 import PdfGenerate from "./PdfGenerate";
+import { useNavigate } from "react-router-dom";
 import { useReactToPrint } from "react-to-print";
+import toast from "react-hot-toast";
 
 const PdfReport = () => {
 	const reportRef = useRef();
+	const navigate = useNavigate();
 
 	const handlePrint = useReactToPrint({
 		content: () => reportRef.current,
 		documentTitle: "Report",
+		onAfterPrint: () => {
+			toast.success("Report downloaded successfully!");
+			navigate("/");
+		},
 	});
 
 	return (
 		<div className="p-6 w-[834px] mx-auto">
-			<div onClick={handlePrint} className="mb-5">
-				<Button btnTitle="Download report" />
+			<div className="flex items-center gap-2 mb-5">
+				<div onClick={() => navigate("/result-and-report")}>
+					<Button btnTitle="Previous" />
+				</div>
+				<div onClick={handlePrint}>
+					<Button btnTitle="Download report" />
+				</div>
 			</div>
 			<div className="border-2 border-[#c4c4c4da] w-[834px]">
 				<PdfGenerate
